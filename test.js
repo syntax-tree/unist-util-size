a8a37a180dab941c793989dd7e5b19bef20e03fc
Titus Wormer <tituswormer@gmail.com>
2021-07-21 16:40:46 +0200

    Fix tests for changes in `@types/unist`

--- test.js
+++ test.js
@@ -16,7 +16,9 @@ test('unist-util-size', function (t) {
 
   t.equal(size(tree), 11, 'complete tree')
   t.equal(size(tree.children[0]), 7, 'parent node with mixed children')
+  // @ts-expect-error: fine.
   t.equal(size(tree.children[0].children[1]), 1, 'parent of text')
+  // @ts-expect-error: fine.
   t.equal(size(tree.children[0].children[0]), 0, 'text node')
 
   t.equal(size(tree, 'element'), 5, 'test')
