7959f779acf001215eef26560340b634dd131d4d
Titus Wormer <tituswormer@gmail.com>
2022-06-05 16:56:41 +0200

    Add improved jsdoc

--- index.js
+++ index.js
@@ -9,13 +9,18 @@
 
 import {convert} from 'unist-util-is'
 
-/** @type {Array.<Node>} */
+/** @type {Array<Node>} */
 let empty
 
 /**
+ * Calculate the number of nodes in `node`.
+ *
  * @param {Node} node
+ *   Tree to traverse.
  * @param {null|undefined|Type|Props|TestFunctionAnything|Array<Type|Props|TestFunctionAnything>} [test]
+ *   `unist-util-is`-compatible test (such as a node type).
  * @returns {number}
+ *   Number of exclusive descendants that pass `test` in `tree`.
  */
 export function size(node, test) {
   const is = convert(test)
@@ -26,7 +31,7 @@ export function size(node, test) {
    * @param {Node} node
    */
   function fastSize(node) {
-    /** @type {Array.<Node>} */
+    /** @type {Array<Node>} */
     // @ts-expect-error Looks like a parent.
     const children = (node && node.children) || empty
     let count = 0
