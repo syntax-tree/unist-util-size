84272f7d4b82fb6ed3b505b89b0645127ec9237a
Titus Wormer <tituswormer@gmail.com>
2023-07-03 12:14:35 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -2,14 +2,11 @@ import assert from 'node:assert/strict'
 import test from 'node:test'
 import {h} from 'hastscript'
 import {size} from './index.js'
-import * as mod from './index.js'
 
-test('size', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['size'],
-    'should expose the public api'
-  )
+test('size', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), ['size'])
+  })
 
   const tree = h('div', [
     h('p', [
@@ -22,13 +19,16 @@ test('size', () => {
     h('pre', h('code', 'bar()'))
   ])
 
-  assert.equal(size(tree), 11, 'complete tree')
   const p = tree.children[0]
   assert(p)
   assert(p.type === 'element')
-  assert.equal(size(p), 7, 'parent node with mixed children')
-  assert.equal(size(p.children[1]), 1, 'parent of text')
-  assert.equal(size(p.children[0]), 0, 'text node')
 
-  assert.equal(size(tree, 'element'), 5, 'test')
+  await t.test('should work', async function () {
+    assert.equal(size(tree), 11)
+
+    assert.equal(size(p), 7, 'parent node with mixed children')
+    assert.equal(size(p.children[1]), 1, 'parent of text')
+    assert.equal(size(p.children[0]), 0, 'text node')
+    assert.equal(size(tree, 'element'), 5, 'test')
+  })
 })
